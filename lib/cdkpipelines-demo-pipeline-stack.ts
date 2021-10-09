ef016171ebf8ff2dae46763d6db21daf255ffa7a
Rajesh Ghosh <rajesh.ghosh.here@gmail.com>
2021-10-09 20:54:42 +0530

    changed pipeline name

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -17,7 +17,7 @@ import { ShellScriptAction } from '@aws-cdk/pipelines';
 
       const pipeline = new CdkPipeline(this, 'Pipeline', {
         // The pipeline name
-        pipelineName: 'HRBotPipeline3',
+        pipelineName: 'HRBotPipeline4',
         cloudAssemblyArtifact,
 
         // Where the source can be found
